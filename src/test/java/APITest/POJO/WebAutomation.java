397d10bfc43def958280f560bfa3d40326814e9c
puttarajbb11@gmail.com <puttarajbb11@gmail.com>
2025-02-10 16:04:45 +0530

    Basic Java Program

---
+++ src/test/java/APITest/POJO/WebAutomation.java
@@ -0,0 +1,25 @@
+package APITest.POJO;
+
+public class WebAutomation {
+
+    private String courseTitle;
+    private String price;
+
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCoursesTile(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    // Getter and Setter for price
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
