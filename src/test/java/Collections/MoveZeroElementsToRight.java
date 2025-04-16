84d4023bbacb77ad20fb4f5262741b2e742ffbbd
Puttaraja <puttarajbb11@gmail.com>
2025-04-16 10:36:32 +0530

    MoveZeroElementToRight

---
+++ src/test/java/Collections/MoveZeroElementsToRight.java
@@ -0,0 +1,10 @@
+package Collections;
+
+import java.util.List;
+
+public class MoveZeroElementsToRight {
+
+    public static void main(String[] args) {
+        List<Integer> list=List.of(1,0,3,-4-1,7,0,8,-2,0,0);
+    }
+}
