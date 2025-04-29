ebffc2d1aaba628ac713db02ed2632bcfe0f9c11
Puttaraja <puttarajbb11@gmail.com>
2025-04-29 11:31:55 +0530

    Interview Question

---
+++ src/test/java/Arrays/Demo1.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+        int[] arr= {50, 10, 25, 1, 17, 99, 33};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    }
+
